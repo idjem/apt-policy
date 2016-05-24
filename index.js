04db7a7cc3740ddcc1a07908e88c838f599c4064
Mejdi Ourir <m.ourir@ivsweb.com>
2016-05-24 12:15:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var cp = require('child_process');
+
+module.exports = function(packages , chain){
+  packages = packages || [];
+  packages = typeof packages == "string" ? [packages] : packages;
+  chain = chain || Function.prototype;
+  var packageVersion = {};
+  var pross = cp.spawn("apt-cache" ,["policy"].concat(packages));
+  var data= '';
+  pross.stdout.on('data', (d) => {
+    data += d;
+  });
+
+  pross.on("error", function(e){
+    chain(e);
+  });
+
+  pross.on('close', (code) => {
+    var z = data.split("\n");
+    packages.forEach(function(package){
+      var t = z.indexOf(package + ":");
+      if(t !== -1)
+        packageVersion[package] = {Installed : z[t+1].split(": ")[1] , Candidate : z[t+2].split(": ")[1]};
+      else
+        packageVersion[package] = {Installed : "not in apt cache" , Candidate : "NOT IN APT CACHE"}
+    });
+    chain(null , packageVersion);
+  });
+
+};
+    
